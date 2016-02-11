4c3e8ed4ac4cca5306215f76c1f05c41d028c20e
Shayan <shayandhanani@gmail.com>
2016-02-11 12:56:18 -0600

    styles

--- src/js/app.js
+++ src/js/app.js
@@ -20,11 +20,15 @@ function toggleMenu() {
 
 			$('.nav__main a').removeClass('active');
 
+			const fade_in_section = '.'+this.innerText.toLowerCase();
 
-			$('.'+this.innerText.toLowerCase()).fadeIn();
-			$(this).addClass('active');
+			$(fade_in_section).fadeIn("slow", function () {
+				$('html, body').animate({
+					scrollTop: $(fade_in_section).offset().top
+				}, 300);
+				$(this).addClass('active');
+			});
 		}
-		
 	})
 }
 
@@ -34,9 +38,22 @@ function getInstagramImages() {
 									userId: '2233485228',
 									accessToken: '2233485228.745c638.53ad455a211e4250a9e0bbc34ff3b0ac',
 									resolution: 'low_resolution',
-									template: '<a href="{{link}}" target="_blank" title="{{caption}}"><img src="{{image}}"/></a>'
-
+									template: '<div class="doughnut"><a href="{{link}}"" target="_blank"><img src="{{image}}"/></a></div>',
+									after: function () {
+										masonryInit();
+									}
 	})
 
 	feed.run();
+}
+
+function masonryInit() {
+	var $instafeed = $('#instafeed').masonry({
+		itemSelector: '.doughnut',
+		fitWidth: true
+	}); 
+
+	$instafeed.imagesLoaded().progress( function() {
+	  $instafeed.masonry('layout');
+	});
 }
